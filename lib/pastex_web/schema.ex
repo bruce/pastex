b3bcf9edc117f189c9c03d8ff01a4c566e7e5d18
Ben Wilson <benwilson512@gmail.com>
2018-09-03 19:31:00 -0700

    basic hard coded seeds

--- lib/pastex_web/schema.ex
+++ lib/pastex_web/schema.ex
@@ -7,5 +7,54 @@ defmodule PastexWeb.Schema do
         {:ok, "up"}
       end)
     end
+
+    field :pastes, list_of(:paste) do
+      resolve &list_pastes/3
+    end
+  end
+
+  object :paste do
+    field :name, non_null(:string)
+    field :description, :string
+    field :files, non_null(list_of(:file))
+  end
+
+  object :file do
+    field :name, :string
+    field :body, :string
+  end
+
+  @pastes [
+    %{
+      name: "Hello World",
+      files: [%{body: ~s[IO.puts("Hello World")]}]
+    },
+    %{
+      name: "Help!",
+      description: "I don't know what I'm doing!",
+      files: [
+        %{
+          name: "foo.ex",
+          body: """
+          defmodule Foo do
+            def foo, do: Bar.bar()
+          end
+          """
+        },
+        %{
+          name: "bar.ex",
+          body: """
+          defmodule Bar do
+            def bar, do: Foo.foo()
+          end
+          """
+        }
+      ]
+    }
+  ]
+
+  @spec list_pastes(any(), any(), any()) :: {:ok, [%{files: [...], name: <<_::88>>}, ...]}
+  def list_pastes(_, _, _) do
+    {:ok, @pastes}
   end
 end
