014992ed31e034895de20f843341a3a008863e2e
Ben Wilson <benwilson512@gmail.com>
2018-09-03 19:35:47 -0700

    fun with resolvers

--- lib/pastex_web/schema.ex
+++ lib/pastex_web/schema.ex
@@ -16,45 +16,71 @@ defmodule PastexWeb.Schema do
   object :paste do
     field :name, non_null(:string)
     field :description, :string
-    field :files, non_null(list_of(:file))
+
+    field :files, non_null(list_of(:file)) do
+      resolve &get_files/3
+    end
   end
 
   object :file do
-    field :name, :string
+    field :name, :string do
+      resolve fn file, _, _ ->
+        {:ok, file[:name] || "Untitled"}
+      end
+    end
+
     field :body, :string
   end
 
   @pastes [
     %{
+      id: 1,
       name: "Hello World",
-      files: [%{body: ~s[IO.puts("Hello World")]}]
+      files: [1]
     },
     %{
+      id: 2,
       name: "Help!",
       description: "I don't know what I'm doing!",
-      files: [
-        %{
-          name: "foo.ex",
-          body: """
-          defmodule Foo do
-            def foo, do: Bar.bar()
-          end
-          """
-        },
-        %{
-          name: "bar.ex",
-          body: """
-          defmodule Bar do
-            def bar, do: Foo.foo()
-          end
-          """
-        }
-      ]
+      files: [2, 3]
     }
   ]
 
-  @spec list_pastes(any(), any(), any()) :: {:ok, [%{files: [...], name: <<_::88>>}, ...]}
   def list_pastes(_, _, _) do
     {:ok, @pastes}
+  end
+
+  @files [
+    %{
+      id: 1,
+      body: ~s[IO.puts("Hello World")]
+    },
+    %{
+      id: 2,
+      name: "foo.ex",
+      body: """
+      defmodule Foo do
+        def foo, do: Bar.bar()
+      end
+      """
+    },
+    %{
+      id: 3,
+      name: "bar.ex",
+      body: """
+      defmodule Bar do
+        def bar, do: Foo.foo()
+      end
+      """
+    }
+  ]
+
+  def get_files(paste, _, _) do
+    files =
+      for id <- paste.files do
+        Enum.find(@files, fn file -> file.id == id end)
+      end
+
+    {:ok, files}
   end
 end
